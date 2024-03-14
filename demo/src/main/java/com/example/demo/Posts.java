538b0191537aefc3b3167222fe3703ff0c465a97
sreyly-long <longsreyly85@gmail.com>
2024-03-14 22:32:42 +0700

    insert post

---
+++ demo/src/main/java/com/example/demo/Posts.java
@@ -0,0 +1,77 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Posts {
+    static int idCounter = 1;
+
+    private int id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime creationDate;
+    private List<String> tags;
+
+    public Posts(int id, String title, String content, String author, LocalDateTime creationDate, List<String> tags) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.creationDate = creationDate;
+        this.tags = tags;
+    }
+
+    public Posts() {
+        this.creationDate = LocalDateTime.now();
+    }
+
+    // Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
+
+
+
