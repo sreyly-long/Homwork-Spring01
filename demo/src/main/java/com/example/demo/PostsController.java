e3dc60d1fb970e379ff3f69afd99beb74b601903
sreyly-long <longsreyly85@gmail.com>
2024-03-15 23:14:48 +0700

    Read post by author

--- demo/src/main/java/com/example/demo/PostsController.java
+++ demo/src/main/java/com/example/demo/PostsController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -25,6 +26,36 @@ public class PostsController {
     public ResponseEntity<List<Posts>> getAllPosts() {
         return ResponseEntity.ok(posts);
     }
+    // Read post by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Posts> getPostByID(@PathVariable int id) {
+        Optional<Posts> optionalPosts = posts.stream().filter(po -> po.getId() == id)
+                .findFirst();
+        return optionalPosts.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+    // Read post by title
+    @GetMapping("/searchByTitle")
+    public ResponseEntity<Posts> getPostByTitle(@RequestParam String title) {
+        Optional <Posts> optionalPosts = posts.stream().filter(po -> po.getTitle().equalsIgnoreCase(title))
+                .findFirst();
+        return optionalPosts.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+    // Read post by author
+    public ResponseEntity<Posts> getPostByAuthor(@RequestParam String author) {
+        List<Posts> foundPosts = new ArrayList<>();
+        for (Posts posts1 : posts) {
+            if (posts1.getAuthor().equalsIgnoreCase(author)) {
+                foundPosts.add(posts1);
+            }
+        }
+        if (foundPosts.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok((Posts)foundPosts);
+    }
+
 
 
 
